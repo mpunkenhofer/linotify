ae50388ab2b3764a6dff442b02b5d73087344bc7
Mathias Punkenhofer <mpunkenhofer@gmail.com>
2020-04-28 14:01:33 +0200

    feat: feature manually adding users closes #2

--- src/options/options.ts
+++ src/options/options.ts
@@ -1,9 +1,10 @@
-import { enableStorageApiLogger, getPreferences, toggleNotificationsEnabled, toggleDisplayBadgeTextEnabled, getUsers, removeUser, toggleNotifyWhenOnline, toggleNotifyWhenPlaying } from "../common/storage";
+import { enableStorageApiLogger, getPreferences, toggleNotificationsEnabled, toggleDisplayBadgeTextEnabled, getUsers, removeUser, toggleNotifyWhenOnline, toggleNotifyWhenPlaying, addUser } from "../common/storage";
 import pkg from "../../package.json"
 import { i18n } from "../constants/i18n";
 import { User } from "../types";
-import { debounce } from "lodash";
+import { debounce, isEmpty } from "lodash";
 import { sortByName } from "../common/util";
+import { getUserStatus } from "../common/lichess";
 
 console.log('LiNotify is open source! https://github.com/mpunkenhofer/linotify');
 
@@ -104,7 +105,7 @@ const createNotifyWhenOnlineSettingCell = (user: User): HTMLElement => {
     const button = document.createElement('a');
     button.classList.add('notify-online-btn', user.notifyWhenOnline ? 'check-icon' : 'times-icon');
     button.onclick = (): void => {
-        if(button.classList.contains('check-icon')) {
+        if (button.classList.contains('check-icon')) {
             button.classList.replace('check-icon', 'times-icon');
         } else {
             button.classList.remove('times-icon');
@@ -121,7 +122,7 @@ const createNotifyWhenPlayingSettingCell = (user: User): HTMLElement => {
     const button = document.createElement('a');
     button.classList.add('notify-playing-btn', user.notifyWhenPlaying ? 'check-icon' : 'times-icon');
     button.onclick = (): void => {
-        if(button.classList.contains('check-icon')) {
+        if (button.classList.contains('check-icon')) {
             button.classList.replace('check-icon', 'times-icon');
         } else {
             button.classList.remove('times-icon');
@@ -147,62 +148,69 @@ const createRemoveUserCell = (user: User, onRemove: (id?: string) => void): HTML
     return button;
 }
 
+const refreshTable = (): void => {
+    getUsers()
+        .then(users => {
+            const table = document.getElementById('user-table');
+            if (table) {
+                // eslint-disable-next-line @typescript-eslint/no-use-before-define
+                table.firstChild?.replaceWith(createTable(users))
+            }
+        })
+        .catch(err => console.error(err));
+}
+
+const createTable = (users: User[]): HTMLElement => {
+    updateUserCount(users.length);
+
+    if (users.length < 1)
+        return document.createElement('div');
+
+    const table = document.createElement('table');
+    table.classList.add('user-table', 'table');
+    const tableHead = table.createTHead();
+
+    if (tableHead) {
+        const row = tableHead.insertRow();
+
+        const nameHeader = document.createElement('th');
+        nameHeader.scope = 'col';
+        nameHeader.appendChild(document.createTextNode(i18n.name));
+
+        const noHeader = document.createElement('th');
+        noHeader.scope = 'col';
+        noHeader.appendChild(document.createTextNode(i18n.notifyWhenOnlineQuestion));
+
+        const npHeader = document.createElement('th');
+        npHeader.scope = 'col';
+        npHeader.appendChild(document.createTextNode(i18n.notifyWhenPlayingQuestion));
+
+        const emptyHeader = document.createElement('th');
+        emptyHeader.scope = 'col';
+
+        row.appendChild(nameHeader);
+        row.appendChild(noHeader);
+        row.appendChild(npHeader);
+        // insert empty cell for last col
+        row.appendChild(emptyHeader);
+    }
+
+    for (const user of sortByName(users)) {
+        const row = table.insertRow();
+
+        row.insertCell().appendChild(createUserCellElement(user));
+        row.insertCell().appendChild(createNotifyWhenOnlineSettingCell(user));
+        row.insertCell().appendChild(createNotifyWhenPlayingSettingCell(user));
+        row.insertCell().appendChild(createRemoveUserCell(user, refreshTable));
+    }
+
+    return table;
+}
+
 const createUserTable = (): HTMLElement => {
     const wrapper = document.createElement('div');
     const tableWrapper = document.createElement('div');
-
-    const createTable = (users: User[]): HTMLElement => {
-        updateUserCount(users.length);
-
-        if(users.length < 1)
-            return document.createElement('div');
-
-        const table = document.createElement('table');
-        table.classList.add('user-table', 'table');
-        const tableHead = table.createTHead();
-
-        if(tableHead) {
-            const row = tableHead.insertRow();
-            
-            const nameHeader = document.createElement('th');
-            nameHeader.scope = 'col';
-            nameHeader.appendChild(document.createTextNode(i18n.name));
-
-            const noHeader = document.createElement('th');
-            noHeader.scope = 'col';
-            noHeader.appendChild(document.createTextNode(i18n.notifyWhenOnlineQuestion));
-
-            const npHeader = document.createElement('th');
-            npHeader.scope = 'col';
-            npHeader.appendChild(document.createTextNode(i18n.notifyWhenPlayingQuestion));
-
-            const emptyHeader = document.createElement('th');
-            emptyHeader.scope = 'col';
-            
-            row.appendChild(nameHeader);
-            row.appendChild(noHeader);
-            row.appendChild(npHeader);
-            // insert empty cell for last col
-            row.appendChild(emptyHeader);
-        }
-
-        const updateTable = (): void => {
-            getUsers()
-                .then(users => tableWrapper.firstChild?.replaceWith(createTable(users)))
-                .catch(err => console.error(err));
-        }
-
-        for (const user of sortByName(users)) {
-            const row = table.insertRow();
-
-            row.insertCell().appendChild(createUserCellElement(user));
-            row.insertCell().appendChild(createNotifyWhenOnlineSettingCell(user));
-            row.insertCell().appendChild(createNotifyWhenPlayingSettingCell(user));
-            row.insertCell().appendChild(createRemoveUserCell(user, updateTable));
-        }
-
-        return table;
-    }
+    tableWrapper.id = 'user-table';
 
     const onInput = (content: string): void => {
         const term = content.toLowerCase();
@@ -226,19 +234,110 @@ const createUserTable = (): HTMLElement => {
     return wrapper;
 }
 
-const createBoxWrapper = (title: string, ...elements: HTMLElement[]): HTMLElement => {
+const createAddUserElement = (): HTMLElement => {
+    const inputGroup = document.createElement('div');
+    inputGroup.classList.add('input-group', 'mb-3');
+
+    const input = document.createElement('input');
+    input.classList.add('form-control')
+    input.type = 'text';
+    input.placeholder = i18n.username;
+
+    const inputGroupAppend = document.createElement('div');
+    inputGroupAppend.classList.add('input-group-append');
+
+    const button = document.createElement('button');
+    button.classList.add('btn', 'btn-primary');
+    button.type = 'button';
+    button.disabled = true;
+    button.textContent = i18n.add;
+
+    const invalidFeedback = document.createElement('div');
+    invalidFeedback.classList.add('invalid-feedback');
+    invalidFeedback.textContent = i18n.invalidUsernameFeedback;
+
+    input.oninput = (ev: Event): void => {
+        if (ev.target) {
+            const el = ev.target as HTMLInputElement;
+
+            if (el.classList.contains('is-invalid')) {
+                el.classList.remove('is-invalid');
+            }
+
+            if (el.value && el.value.length > 1) {
+                button.disabled = false;
+            } else {
+                button.disabled = true;
+            }
+        }
+    }
+
+    input.onkeyup = (ev: Event): void => {
+        if ((ev as KeyboardEvent).keyCode === 13) {
+            // Cancel the default action, if needed
+            ev.preventDefault();
+            button.click();
+        }
+    }
+
+    const spinner = document.createElement('span');
+    spinner.classList.add('spinner-border', 'spinner-border-sm')
+
+
+    button.onclick = (): void => {
+        button.textContent = '';
+        button.appendChild(spinner);
+        button.appendChild(document.createTextNode(` ${i18n.checking}`));
+
+        getUserStatus(input.value || '')
+            .then((statusResponse) => {
+                console.log(statusResponse);
+                if (!isEmpty(statusResponse)) {
+                    if (statusResponse[0].id !== undefined) {
+                        addUser(statusResponse[0].id)
+                        //location.reload();
+                        input.value = '';
+                        refreshTable();
+                    }
+                } else {
+                    input.classList.add('is-invalid');
+                    inputGroup.appendChild(invalidFeedback);
+                }
+            })
+            .catch(err => console.log(err))
+            .finally(() => {
+                button.innerHTML = '';
+                button.textContent = i18n.add;
+            });
+    }
+
+    inputGroupAppend.appendChild(button);
+    inputGroup.appendChild(input);
+    inputGroup.appendChild(inputGroupAppend);
+
+    return inputGroup;
+}
+
+const createBoxWrapper = (title: string, description: string, ...elements: HTMLElement[]): HTMLElement => {
     const box = document.createElement('div');
     box.classList.add('bg-light', 'border', 'py-2', 'py-md-3', 'px-2', 'px-md-3', 'mr-1', 'mr-md-2', 'mr-lg-4', 'my-1', 'my-md-3', 'my-lg-5', 'col');
 
-    if (title && title.length > 0) {
+    if (title.length > 0) {
         const titleElement = document.createElement('h2');
         titleElement.classList.add('h4');
         titleElement.innerText = title;
         box.appendChild(titleElement);
     }
 
+    if (description.length > 0) {
+        const descriptionElement = document.createElement('p');
+        //descriptionElement.classList.add('text-muted');
+        descriptionElement.innerText = description;
+        box.appendChild(descriptionElement);
+    }
+
     const content = document.createElement('div');
-    content.classList.add('d-flex', 'flex-column', 'py-3');
+    content.classList.add('d-flex', 'flex-column', 'pt-2', 'pb-3');
 
     for (const element of elements) {
         content.appendChild(element);
@@ -260,7 +359,7 @@ if (content) {
     getPreferences()
         .then(prefs => {
             content.appendChild(
-                createBoxWrapper(i18n.settings,
+                createBoxWrapper(i18n.settings, '',
                     createSwitch({
                         title: i18n.enableSystemNotificationsSettingTitle,
                         description: i18n.enableSystemNotificationsSettingDescription,
@@ -276,7 +375,8 @@ if (content) {
                 )
             );
 
-            content.appendChild(createBoxWrapper(i18n.users, createUserTable()));
+            content.appendChild(createBoxWrapper(i18n.users, i18n.userListDescription, createUserTable()));
+            content.appendChild(createBoxWrapper(i18n.addUser, i18n.addUserDescription, createAddUserElement()));
         })
         .catch(err => console.error(err));
 } 
